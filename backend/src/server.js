de93939e607ca51d82df172ac26a44f08c4787d7
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-12-08 12:45:06 -0300

    chore: setup inicial do backend

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+require("express-async-error");
+
+const express = require("express");
+
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/test", (req, res) => {
+  res.send("The server is running!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
